0ca2d7a2779096573db8cda6dde202da57b28fb1
Boris Semyonov <borisemyonov@gmail.com>
2025-10-05 00:29:13 +0200

    New forms

---
+++ scripts/gsc_subscribe.js
@@ -0,0 +1,6 @@
+const gscDiv = document.getElementById('gsc-subscribe');
+gscDiv.innerHTML = '<h2>Subscribe</h2>' +
+    '<p class="center">Subscribe to Randobytes newsletter to stay updated and be among the first players!</p>' +
+    '<div data-getsitecontrol-inline="508726"></div>' +
+    '<p class="center small">No spam, just the good stuff. Unsubscribe at any time.</p>' +
+    '<script type="text/javascript" async src="//l.getsitecontrol.com/p7jn90m4.js"></script>';
