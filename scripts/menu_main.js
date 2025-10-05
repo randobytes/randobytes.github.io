0cea556e413c17f6c5546f4531ef914d1d138df0
Boris Semyonov <borisemyonov@gmail.com>
2025-10-05 19:49:04 +0200

    Social items

--- scripts/menu_main.js
+++ scripts/menu_main.js
@@ -1,15 +1,25 @@
 const topMenu = document.getElementById('top-menu');
 
-topMenu.innerHTML = '<div class="top-menu-content">' +
-    '<div class="column"><a href="index.html" class="menu-item-home"><img src="favicon32.png" /><span>RANDOBYTES</span></a></div>' +
+topMenu.innerHTML =
+    '<div class="top-menu-content">' +
 
-    '<div class="column, right, dropdown"><a href="javascript:void(0)" class="dropdown-title">Games</a>' +
+    '<a href="index.html" class="menu-item-home"><img src="favicon32.png" /><span>RANDOBYTES</span></a>' +
+
+    '<span class="column"></span>' +
+
+    '<div class="top-menu-items">' +
+    '<div class="dropdown">' +
+    '<a href="javascript:void(0)" class="dropdown-title">Games</a>' +
     '<div class="dropdown-content">' +
     '<a href="galaxyz/index.html" class="menu-item"><img src="galaxyz/images/icon.png" /><span>Galaxyz</span></a>' +
     '<a href="wonder_surprise/index.html" class="menu-item"><img src="wonder_surprise/images/icon.png" /><span>Wonder Surprise</span></a>' +
-    '</div></div>' +
+    '</div>' +
+    '</div>' +
 
-    '<div class="column, right, dropdown"><a href="about.html" class="dropdown-title">About</a></div>' +
+    '<div class="dropdown">' +
+    '<a href="about.html" class="dropdown-title">About</a>' +
+    '</div>' +
+    '</div>' +
 
     '</div>';
 
